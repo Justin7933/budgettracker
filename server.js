a456dcc36e8a489a613b2871d156ee9afd9bf6dc
Justin Damon <justindamon84@gmail.com>
2021-11-17 19:14:01 -0500

    mongoose fix & heroku deploy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || process.env.DB_MONGODB, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budgettracker' , {
   useNewUrlParser: true,
 });
 
